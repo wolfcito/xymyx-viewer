095c4e9283dc8eb9548b53251edd6d4880e1a7f4
Luis Fernando <guffenix@gmail.com>
2024-12-08 17:19:00 -0500

    feat: xymyx types added

---
+++ src/app/types/xymyx.type.ts
@@ -0,0 +1,12 @@
+export interface XymyxPiece {
+  type: 'king' | 'queen' | 'rook' | 'bishop' | 'knight' | 'pawn'
+  color: 'white' | 'black'
+  position: {
+    row: number
+    col: number
+  }
+}
+
+export interface XymyxBoardState {
+  pieces: XymyxPiece[]
+}
