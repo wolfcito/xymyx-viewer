50c58619d8f2f24c23f4cdf213873dc388695fbc
Luis Fernando <guffenix@gmail.com>
2024-12-08 20:48:12 -0500

    fix: simple style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-200`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-zinc-200 text-black`}
       >
         {children}
       </body>
